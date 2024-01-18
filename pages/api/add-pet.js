b7782b91d70af739ffc49ac7962aa810c7a6faf3
RyanBagusB <134161510+RyanBagusB@users.noreply.github.com>
2024-01-18 22:56:03 +0700

    Update add-pet.js

--- pages/api/add-pet.js
+++ pages/api/add-pet.js
@@ -32,7 +32,7 @@ export default async function handler(req, res) {
     }
 
     if (req.method === 'DELETE') {
-      const { petId } = req.body;
+      const petId = 1;
       if (!petId) {
         return res.status(400).json({ error: 'Pet ID is required for deletion' });
       }
