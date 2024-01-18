7895f70b1c89dbda7fc9897bca8ef31d8ae363ab
RyanBagusB <134161510+RyanBagusB@users.noreply.github.com>
2024-01-18 23:21:48 +0700

    Update add-pet.js

--- pages/api/add-pet.js
+++ pages/api/add-pet.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
     if (req.method === 'DELETE') {
       const { petName, ownerName } = req.body;
 
-      await sql`DELETE FROM Pets WHERE Name`;
+      await sql`DELETE FROM Pets`;
       // if (deleteResult.rowCount === 0) {
       //   return res.status(404).json({ error: 'Pet not found for deletion' });
       // }
